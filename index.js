a4364003f4ade6d7f4dd39e1ded51eca7d53239a
Michael Orvin <morvin@MacBook-Pro.local>
2021-10-08 10:54:54 -0600

    successful pass on task 6

--- index.js
+++ index.js
@@ -295,9 +295,11 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array, id, name, years, genre, nationality, bio) {
+  array.push({id: id, name: name, years: years, genre: genre, nationality: nationality, bio: bio});
+  return array;
 }
+console.log('task 6:', addArtist(artists, '20', 'Mike Orvin', '1992-2021', 'Web Design', 'Spanish', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'));
 
 
 
@@ -308,7 +310,7 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/) {
+function lotsOfArt(array, names) {
   /*Your Code Here*/
 }
 
