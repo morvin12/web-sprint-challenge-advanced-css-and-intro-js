48ec20786dfc8e67538a1dc9a9e1c166d1b4038b
Michael Orvin <morvin@MacBook-Pro.local>
2021-10-08 10:16:43 -0600

    finished tasks 1-5, still need to trouble shoot on the terminal

--- index.js
+++ index.js
@@ -229,9 +229,11 @@ Use getArtistByIndex to do the following:
 
 Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array, number) {
+  return `the artists at index ${array[number].id} is ${array[number].name}`
 }
+
+console.log('task 3:', getArtistByIndex(artists, 0));
 
 
 
@@ -244,10 +246,17 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
+function get20s(array) {
+  let twentyCentury = []
+  for(let i = 0; i < array.length; i++){
+    if(artists[i].years >= 1900){
+      twentyCentury.push(artists[i].name)
+    }
+  }
+  return twentyCentury;
 }
 
+console.log('task 4:', get20s(artists));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -259,9 +268,15 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(array, number) {
+  for(let i = 0; i < array.length; i++)
+  if(array[i] === number) {
+    array.splice(i, 1);
+  }
+  return array.length
 }
+
+console.log('task 5:', removeArtist(artists, 0));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
